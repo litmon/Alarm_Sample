9dac12687a1539bebe1c74aa4b25ba5e56f7e8b4
Fukuo Kadota <app.fk91@gmail.com>
2014-11-28 16:08:01 +0900

    Initial commit

---
+++ app/src/main/java/com/app/mon/alerm_sample/AlarmBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.app.mon.alerm_sample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by FukuoKadota on 2014/11/28.
+ */
+public class AlarmBroadcastReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "アラームが呼ばれたよ！", Toast.LENGTH_SHORT).show();
+    }
+}
